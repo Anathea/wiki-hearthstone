e5e23bcd174edccedd390b1db1634158ae99fbbf
Solène Fernandez Serrano <Anathea@users.noreply.github.com>
2016-12-14 11:25:13 +0100

    Ajout d'un commentaire

--- PHP-brouillon/db.php
+++ PHP-brouillon/db.php
@@ -1,8 +1,11 @@
 <?php
 $db = 'Wiki-hs';
 $host = 'localhost';
+// Le username et password n'est peut-être pas le même pout vous
+// Ça peut être root root (comme pour Yann)
 $username = 'admin';
 $password = 'admin';
+
 try{
     return new PDO("mysql:dbname=$db;host=$host", $username, $password);
 } catch(PDOException $exception) {
