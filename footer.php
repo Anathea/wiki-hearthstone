53573e33e5dfcc483516ac6150729d8c1532a5d9
leovdw <leopold.von-der-weid@hetic.net>
2016-12-15 19:26:38 +0100

    header/footer

---
+++ footer.php
@@ -0,0 +1,37 @@
+
+</main>
+<footer><a href="ajoutcarte.php">Ajouter une carte</a></footer>
+</body>
+</html>
+
+<script>
+        var filterClasse = document.querySelectorAll('#Select input');
+
+        for(var i = 0; i < filterClasse.length; i++) {
+            filterClasse[i].addEventListener('click', function(e) {
+                var filterClasse = document.querySelectorAll('#Select input');
+                for (var i = 0 ; i < filterClasse.length ; i++) {
+                   filterClasse[i].checked = false;
+                }
+                this.checked = true;
+                changeClasse(this.getAttribute('id'));
+            })
+        }
+
+        var changeClasse = function (classe) {
+            var cartes = document.querySelectorAll('.cartes');
+            for (var i = 0; i < cartes.length ; i++) {
+                var carteClasse = cartes[i].getAttribute('data-classe');
+                if (carteClasse == classe) {
+                    cartes[i].style.display = 'inline-block';
+                } else {
+                    cartes[i].style.display = 'none';
+                }
+
+                if (classe === 'all') {
+                    cartes[i].style.display = 'inline-block';
+                }
+            }
+        }
+
+    </script>
