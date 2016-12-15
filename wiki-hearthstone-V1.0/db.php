5b7633c85c1d97e112debac3e3f251127db6ea35
Anathea <solene.fernandez-serrano@hetic.net>
2016-12-15 20:13:17 +0100

    Ajout du zip V-simple

--- wiki-hearthstone-V1.0/db.php
+++ wiki-hearthstone-V1.0/db.php
@@ -3,8 +3,8 @@ $db = 'wiki-hearthstone';
 $host = 'localhost';
 
 // Éventuellement à modifier en 'root' et 'root'
-$username = 'admin';
-$password = 'admin';
+$username = 'root';
+$password = 'root';
 try{
     return new PDO("mysql:dbname=$db;host=$host", $username, $password);
 } catch(PDOException $exception) {
